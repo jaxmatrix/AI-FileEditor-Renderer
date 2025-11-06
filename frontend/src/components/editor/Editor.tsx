75363d6f4bbbb3f4297edc838fd8e3cb6fc7ddf6
Jai Shukla <jay@live.co.in>
2025-11-06 07:08:29 +0530

    Updated editor

--- frontend/src/components/editor/Editor.tsx
+++ frontend/src/components/editor/Editor.tsx
@@ -1,41 +1,53 @@
-import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from '@tiptap/starter-kit';
-import { useSocket } from "@/contexts/SocketContext";
-import { useEffect } from "react";
+import { useEditor, EditorContent } from '@tiptap/react'
+import { BubbleMenu } from '@tiptap/extension-bubble-menu'
+import StarterKit from '@tiptap/starter-kit'
+import { useSocket } from '@/contexts/SocketContext'
+import { useEffect } from 'react'
+import SlashCommand from './slash-command'
 
 const Editor = () => {
-  const { fileContent, editContent, fileId, onFileUpdate } = useSocket();
+  const { fileContent, editContent, fileId, onFileUpdate } = useSocket()
 
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      BubbleMenu.configure({
+        element: document.querySelector('.bubble-menu') as HTMLElement | null,
+      }),
+      SlashCommand,
+    ],
     content: fileContent,
     onUpdate: ({ editor }) => {
       if (fileId) {
-        editContent(fileId, editor.getHTML());
+        editContent(fileId, editor.getHTML())
       }
     },
-  });
+  })
 
   useEffect(() => {
     onFileUpdate((newContent: string) => {
       if (editor && editor.getHTML() !== newContent) {
-        editor.commands.setContent(newContent);
+        editor.commands.setContent(newContent)
       }
-    });
-  }, [editor, onFileUpdate]);
+    })
+  }, [editor, onFileUpdate])
 
   useEffect(() => {
     if (editor && editor.getHTML() !== fileContent) {
-      editor.commands.setContent(fileContent);
+      editor.commands.setContent(fileContent)
     }
-  }, [fileContent, editor]);
+  }, [fileContent, editor])
 
   return (
     <>
+      <div className="bubble-menu">
+        <button>Bold</button>
+        <button>Italic</button>
+        <button>Strike</button>
+      </div>
       <EditorContent editor={editor} />
     </>
-  );
-};
+  )
+}
 
-export default Editor;
-
+export default Editor
